93704a02c6f9cbec6ec32da40c5c4163b9e55840
ojag95 <ojag95@gmail.com>
2018-12-27 14:31:33 -0600

    Implementacion de Registro de Ordenes con AJAX y JSON

--- RestauranteMariscos/src/java/ModeloDAO/ProductoDAO.java
+++ RestauranteMariscos/src/java/ModeloDAO/ProductoDAO.java
@@ -8,9 +8,24 @@ package ModeloDAO;
 import CodeHelpers.ConexionesDB;
 import Interfaces.CRUDProductos;
 import Modelo.Producto;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL; 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ProductoDAO implements CRUDProductos {
 
@@ -124,5 +139,170 @@ public class ProductoDAO implements CRUDProductos {
     public boolean agregar(Producto producto) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    
+    public void obtenerJSONCategorias(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    String tipo="";
+    if(request.getParameter("tipo")==null)
+    {
+    
+    }
+    else{
+        tipo=request.getParameter("tipo");
+    }
+        
+    JsonArray jsonArray = new JsonArray(); 
+    ResultSet resultadoConsulta = null;
+        System.out.println("select Categoria from Producto where tipo='"+tipo+"' group by Categoria");
+            try {
+            resultadoConsulta = conector.consulta("select Categoria from Producto where tipo='"+tipo+"' group by Categoria");
+            while (resultadoConsulta.next()) {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("nombre", resultadoConsulta.getString("Categoria"));
+                jsonArray.add(jsonObject);
+            }  
+        String json =jsonArray.toString();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);    
+        } catch (Exception e) {
+                System.out.println("Error"+e);
+        }
+   }
+    
+    public void obtenerJSONTipos(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    JsonArray jsonArray = new JsonArray(); 
+    ResultSet resultadoConsulta = null;
+            try {
+            resultadoConsulta = conector.consulta("select Tipo from Producto group by Tipo");
+            while (resultadoConsulta.next()) {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("nombre", resultadoConsulta.getString("Tipo"));
+                jsonArray.add(jsonObject);
+            }  
+        String json =jsonArray.toString();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);    
+        } catch (Exception e) {
+                System.out.println("Error"+e);
+        }
+   }
+    
+    public void obtenerJSONProductosXCategorias(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    JsonArray jsonArray = new JsonArray(); 
+    ResultSet resultadoConsulta = null;
+    String categoria="";
+    if(request.getParameter("categoria")==null)
+    {
+    
+    }
+    else{
+        categoria=request.getParameter("categoria");
+    }
+    System.out.println("Atributo "+categoria);
+
+    //String categoria="Coctel";
+            try {
+            resultadoConsulta = conector.consulta("select * from Producto where categoria='"+categoria+"'");
+            while (resultadoConsulta.next()) {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("idProducto", resultadoConsulta.getString("idProducto"));
+                jsonObject.addProperty("nombreProducto", resultadoConsulta.getString("nombreProducto"));
+                jsonObject.addProperty("descripcionProducto", resultadoConsulta.getString("descripcionProducto"));
+                jsonObject.addProperty("precioProducto", resultadoConsulta.getString("precioProducto"));
+                jsonObject.addProperty("tipo", resultadoConsulta.getString("tipo"));
+                jsonObject.addProperty("categoria", resultadoConsulta.getString("categoria"));
+
+
+                jsonArray.add(jsonObject);
+            }  
+        String json =jsonArray.toString();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);    
+        } catch (Exception e) {
+                System.out.println("Error"+e);
+        }
+   }
+    
+    public void obtenerOrden(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    
+    String orden=request.getParameter("orden");
+    String empleado=request.getParameter("empleado");
+    String operacion=request.getParameter("operacion");
+
+    if(orden==null||empleado==null||operacion==null)
+    {
+        System.out.println("La informacion no llego de manera correcta");
+    }
+    else{
+        if(operacion.equals("nueva"))
+        {
+            ResultSet resultado=conector.consulta("select MAX(idOrden) as valor from Orden;");
+            int numeromaximo=0;
+            int numeroOrden=0;
+            while(resultado.next())
+            {
+                numeromaximo=resultado.getInt("valor");                
+            }
+            numeroOrden=numeromaximo+1;
+            
+            JsonParser jsonParser = new JsonParser();
+            JsonArray jsonArray = (JsonArray) jsonParser.parse(orden);
+            String json =jsonArray.toString();
+            //System.out.println(json);
+            int tamanioJson =jsonArray.size();
+            String salida1=conector.registrar("insert into Orden (idOrden,noEmpleado,subtotal,total,estado)values("+numeroOrden+","+empleado+","+0+","+0+",\"abierta\");");
+            System.out.println(salida1);
+                for (int i = 1; i < tamanioJson; i++) {
+                    if(i<tamanioJson-1)
+                    {
+                        //System.out.println(jsonArray.get(i).toString());
+                        JsonObject objetoJson=(JsonObject) jsonArray.get(i);
+                        //System.out.println(objetoJson.get(" ID ").toString());
+                        //System.out.println(objetoJson.get(" ID ").getAsString());
+                        //System.out.println(objetoJson.get("Producto").getAsString());
+                        //System.out.println(objetoJson.get("cantidad").getAsString());
+                        int idProducto=objetoJson.get(" ID ").getAsInt();
+                        int cantidad=objetoJson.get("cantidad").getAsInt();
+                        float precio=obtenerPrecio(idProducto);
+                        float subtotal=precio*cantidad;
+                        System.out.println("id "+idProducto+" precio "+precio+" sutotal "+subtotal );
+
+                        //System.out.println(objetoJson);
+                        
+                        String salida=conector.registrar("insert into OrdenProducto (idOrden,idProducto,subtotal)values("+numeroOrden+","+idProducto+","+subtotal+")");
+                        System.out.println(salida);
+                    }
+
+                }
+        }
+
+
+    }
+        
+    
+   }
+    
+    
+   public float obtenerPrecio(int id)
+   {
+       String idProducto=""+id;
+       float precio=0;
+        try {
+            ResultSet resultado=conector.consulta("select precioProducto from Producto where idProducto="+idProducto+";");
+            while(resultado.next())
+            {
+                precio=resultado.getFloat("precioProducto");
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("No se puedo obtener el precio: "+ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return precio;
+   }
 
 }
---
+++ RestauranteMariscos/web/Orden/ObtenerTipos.jsp
@@ -0,0 +1,17 @@
+<%-- 
+Pagina que retorna un JSON con las categorias de los platillos
+--%>
+
+<%@page import="java.util.Iterator"%>
+<%@page import="java.util.List"%>
+<%@page import="Modelo.Producto"%>
+<%@page import="ModeloDAO.ProductoDAO"%>
+<%@page language="java" contentType="application/json;charset=UTF-8" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%  ProductoDAO dao = new ProductoDAO();
+    dao.obtenerJSONTipos(request, response);
+%>
+    
